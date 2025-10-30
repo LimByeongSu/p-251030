71dff35ba1110b6af0855e20363e4a7e51b0ef81
caketower <dlaqudtn1107@naver.com>
2025-10-30 09:52:16 +0900

    5강 : 반복문

--- src/main/kotlin/day1/exam3/Main.kt
+++ src/main/kotlin/day1/exam3/Main.kt
@@ -5,7 +5,4 @@ fun main() {
     val number = 10
     val str = if(number % 2 == 0) "Even" else "Odd" //3항 연산자와 비슷하다.
     println(str)
-
-
-
 }
---
+++ src/main/kotlin/day1/exam4/Main.kt
@@ -0,0 +1,11 @@
+package day1.exam4
+
+fun main(args: Array<String>) {
+    for(i in 1 until 5){
+        println(i)
+    }
+    for(i in 1 .. 4){
+        println("Count ${i}");
+    }
+
+}
