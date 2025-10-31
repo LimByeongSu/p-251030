f1776080ecdcd938244692031e746fd1e4772b6c
caketower <dlaqudtn1107@naver.com>
2025-10-31 10:38:21 +0900

    40강 : 부 생성자 (Secondary Constructor)

---
+++ src/main/kotlin/day1/exam39/Main.kt
@@ -0,0 +1,10 @@
+package day1.exam39
+
+class Person(val name: String, val age: Int) {
+    constructor(name: String) : this(name, 0) // 주 생성자 호출
+}
+
+fun main() {
+    val person1 = Person("Alice", 25)
+    val person2 = Person("Bob") // age는 기본값 0
+}
